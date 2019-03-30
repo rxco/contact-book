b19d1a1504faf4335c84dca068561f9705f86c81
Raúl Contreras <rxco.ar@gmail.com>
2019-03-30 00:12:58 -0600

    Views and Assets
    This commits includes all the assets ( css, js, images) and views for the frontend of the project.

---
+++ app/assets/javascripts/init.js
@@ -0,0 +1,9 @@
+(function($){
+  $(function(){
+
+    $('.sidenav').sidenav();
+    $('.parallax').parallax();
+    $('.sidenav').sidenav();
+
+  }); // end of document ready
+})(jQuery); // end of jQuery name space
